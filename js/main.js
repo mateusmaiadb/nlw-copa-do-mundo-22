821e250d53b79c1a39171320920973e2a185a731
Mateus Pereira Rodrigues <86198294+mateusmaiadb@users.noreply.github.com>
2022-11-03 23:54:25 -0300

    pondo no js conteudo do card

---
+++ js/main.js
@@ -0,0 +1,25 @@
+document.querySelector("#app").innerHTML = `
+    <header>
+        <img src="./assets/logo.svg" alt="Logo da NLW" />
+    </header>
+        <main id="cards">
+        <div class="card">
+          <h2>
+            24/11
+            <span>quinta</span>
+          </h2>
+          <ul>
+            <li>
+              <img src="./assets/bandeiras-dos-países/icon=brazil.svg" alt="Bandeira do Brasil" />
+              <strong>16:00</strong>
+              <img src="./assets/bandeiras-dos-países/icon=serbia.svg" alt="Bandeira da Sérvia" />
+            </li>
+            <li>
+              <img src="./assets/bandeiras-dos-países/icon=switzerland.svg" alt="Bandeira do Brasil" />
+              <strong>16:00</strong>
+              <img src="./assets/bandeiras-dos-países/icon=cameroon.svg" alt="Bandeira da Sérvia" />
+            </li>
+          </ul>
+        </div>
+        </main>
+`
