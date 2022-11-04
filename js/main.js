0775deebfdcff9cc398b3a56e0ef611515c4046e
Mateus Pereira Rodrigues <86198294+mateusmaiadb@users.noreply.github.com>
2022-11-04 17:16:39 -0300

    Lógica criada para criar os cards

--- js/main.js
+++ js/main.js
@@ -1,25 +1,37 @@
+function createGame(player1, hour, player2, group ) {
+    return `
+    <p>${group}</p>
+      <li>
+        <img src="./assets/bandeiras-dos-países/icon=${player1}.svg" alt="Bandeira do ${player1}" />
+        <strong>${hour}</strong>
+        <img src="./assets/bandeiras-dos-países/icon=${player2}.svg" alt="Bandeira da ${player2}" />
+      </li>
+    `
+}
+
+
+
+function createCard(date, day, games) {
+    return `
+       <div class="card">
+       <h2>${date}<span>${day}</span></h2>
+       
+          <ul>
+            ${games}
+          </ul>
+        </div>
+    `
+}
+
+
 document.querySelector("#app").innerHTML = `
     <header>
         <img src="./assets/logo.svg" alt="Logo da NLW" />
     </header>
         <main id="cards">
-        <div class="card">
-          <h2>
-            24/11
-            <span>quinta</span>
-          </h2>
-          <ul>
-            <li>
-              <img src="./assets/bandeiras-dos-países/icon=brazil.svg" alt="Bandeira do Brasil" />
-              <strong>16:00</strong>
-              <img src="./assets/bandeiras-dos-países/icon=serbia.svg" alt="Bandeira da Sérvia" />
-            </li>
-            <li>
-              <img src="./assets/bandeiras-dos-países/icon=switzerland.svg" alt="Bandeira do Brasil" />
-              <strong>16:00</strong>
-              <img src="./assets/bandeiras-dos-países/icon=cameroon.svg" alt="Bandeira da Sérvia" />
-            </li>
-          </ul>
-        </div>
+        ${createCard("24/11", "quinta", createGame('switzerland', '07:00', 'cameroon', 'Group G'))}
+        ${createCard("24/11", "quinta", createGame('brazil', '16:00', 'serbia', 'Group G'))}
+        ${createCard("28/11", "segunda", createGame('switzerland', '13:00', 'brazil', 'Grupo G'))}
+        ${createCard("02/12", "sexta", createGame('brazil', '16:00', 'cameroon' ,'Grupo G'))}
         </main>
 `
