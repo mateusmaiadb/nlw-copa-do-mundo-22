ae903f870ed2dc93a5d380573c3983e757f65869
Mateus Pereira Rodrigues <86198294+mateusmaiadb@users.noreply.github.com>
2022-11-21 12:16:07 -0300

    atualização

--- js/main.js
+++ js/main.js
@@ -9,13 +9,10 @@ function createGame(player1, hour, player2, details) {
     `
 }
 
-
-
 function createCard(date, day, games) {
     return `
        <div class="card">
        <h2>${date}<span>${day}</span></h2>
-       
           <ul>
             ${games}
           </ul>
@@ -29,7 +26,7 @@ document.querySelector("#app").innerHTML = `
         <img src="./assets/logo.svg" alt="Logo da NLW" />
     </header>
         <main id="cards">
-        ${createCard("24/11", "quinta", createGame('switzerland', '07:00', 'cameroon', 'Group G')
+        ${createCard("24/11", "quinta", createGame('switzerland', '07:00', 'cameroon', 'Grupo G')
         + createGame('brazil', '16:00', 'serbia', '1ª Rodada'))}
         
         ${createCard("28/11", "segunda", createGame('cameroon', '07:00', 'serbia', 'Grupo G')
@@ -37,5 +34,12 @@ document.querySelector("#app").innerHTML = `
 
         ${createCard("02/12", "sexta", createGame('switzerland', '16:00', 'serbia', 'Grupo G')
         + createGame('cameroon', '16:00', 'brazil', '3ª Rodada'))}
+
+        ${createCard("21/11", "segunda", createGame("england", "10:00", "iran", "Grupo B")
+        + createGame("senegal", "13:00", "netherlands", "Grupo A") + createGame("united states", "16:00", "wales", "Grupo B"))}
+
+        ${createCard("22/11", "terça", createGame("argentina", "7:00", "saudi arabia", "Grupo C") + createGame("denmark", "10:00", "tunisia", "Grupo D") + createGame("mexico", "13:00", "poland", "Grupo C") + createGame("france", "16:00", "australia", "Grupo D"))}
+
+        ${createCard("23/11", "quarta", createGame("morocco", "07:00", "croatia", "Grupo F"))}
         </main>
 `
